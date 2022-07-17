671376565cb7dd0037750f84bfaf7ff6a1bc14ac
Danny Rivers <cdeevfrr@gmail.com>
2022-07-17 16:45:16 -0400

    Fix action popup to close after a choice

--- src/PlayerActions.tsx
+++ src/PlayerActions.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNode, useState } from "react";
 import { Tile } from "./MapHelpers";
 import { ActionKey } from "./PlayerActionTypes";
 import Popup from 'reactjs-popup';
@@ -20,13 +20,22 @@ function PlayerActions({tile, playerChoseAction}: {tile: Tile, playerChoseAction
         <div>
             <button onClick={()=> playerChoseAction(ActionKey.DoNothing)}>Do Nothing</button>
             <Popup trigger={<button>Attack</button>} modal>
-            <div style={{backgroundColor: "#ffffff", margin: "5%", }}>
+                {(close => (
+                <div style={{backgroundColor: "#ffffff", margin: "5%", }}>
                 <div>Choose something to target!</div>
-                {
+                
+                    {
                     tile.entities.map(entity => 
-                        <button style={{margin: "5%"}} key={entity.id} onClick={() => playerChoseAction(ActionKey.Attack, {target: entity})}>{entity.displayName}</button>)
-                }
-            </div>
+                        <button style={{margin: "5%"}} key={entity.id} 
+                            onClick={() => {
+                            playerChoseAction(ActionKey.Attack, {target: entity})
+                            close()
+                            }
+                        }>{entity.displayName}</button>)
+                    }
+                </div>
+                // https://stackoverflow.com/questions/68085033/how-can-i-close-a-reactjs-popup-modal-on-submit/73015201#73015201
+                )) as unknown as ReactNode} 
             </Popup>
         </div>
     </div>
