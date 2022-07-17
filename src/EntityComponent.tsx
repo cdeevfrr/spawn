bab562cb00d140ef7eac2f7af0efe22b63e537d0
Danny Rivers <cdeevfrr@gmail.com>
2022-07-17 11:12:25 -0400

    Clarify some css for EntityComponent

--- src/EntityComponent.tsx
+++ src/EntityComponent.tsx
@@ -7,7 +7,8 @@ export {EntityComponent}
 
 function EntityComponent({entity, entityCounter}: {entity: Entity, entityCounter: number, key: string}){
     const left = (entityCounter * (- 6 )) + "%"
-    const top = (-100 + (entityCounter *  6 ) - (53 * entityCounter)) + "%"
+    const tileTop = (-100 + -60 * entityCounter ) // Because this object will be down by 100% for the tile, then 60% for each entity.
+    const top = (tileTop + entityCounter*8 ) + "%"
     return <img 
       style={{position:"relative", top, left, width: "60%", height: "60%", zIndex: entityCounter}} 
       src={imageTable[entity.imageLookupKey]}
